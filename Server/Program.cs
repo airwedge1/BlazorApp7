ead825c8c9d7cc56f4eb8cdf64ce2faf59dac8c0
Nicholas Walker <nwalker@kubicacorp.com>
2022-02-22 12:59:20 -0500

    simplify a little more

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,84 +3,36 @@ using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var useServerSideBlazor = builder.Configuration.GetValue<bool>("UseServerSideBlazor");
 
-if (useServerSideBlazor)
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddServerSideBlazor().AddCircuitOptions(o =>
 {
-    // Add services to the container.
-    builder.Services.AddRazorPages();
-    builder.Services.AddControllersWithViews();
-    builder.Services.AddServerSideBlazor().AddCircuitOptions(o =>
-    {
-        o.DetailedErrors = true;
-    });
+    o.DetailedErrors = true;
+});
 
-    
-    builder.Services.AddScoped(sp => {
-        var baseAddress = "https://localhost:7025";
-
-        return new HttpClient
-        {
-            BaseAddress = new Uri(baseAddress)
-        };
-    });
+   
 
 
-    var app = builder.Build();
+var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    if (!app.Environment.IsDevelopment())
-    {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.MapBlazorHub();
-    app.MapControllers();
-    app.MapFallbackToPage("/_Host");
-
-    app.Run();
-}
-else
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-
-    // Add services to the container.
-
-    builder.Services.AddControllersWithViews();
-    builder.Services.AddRazorPages();
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseWebAssemblyDebugging();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseBlazorFrameworkFiles();
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-
-    app.MapRazorPages();
-    app.MapControllers();
-    app.MapFallbackToPage("/_Host");
-
-    app.Run();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapControllers();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
